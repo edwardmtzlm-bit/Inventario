1ff86a9dc8186593e471b0add112fee372a7ba3a
MacBook <horaciomarchandoficial@MacBook-Air.local>
2026-02-23 09:07:52 -0600

    chore: sync service worker artifact

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a35l2c1a2hc"
+    "revision": "0.8mgevdfakg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
